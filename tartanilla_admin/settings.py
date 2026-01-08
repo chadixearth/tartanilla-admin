df5c9e28d38491f328e5fb052283a59ff2bc169e
chadixearth <richard.legaspi.1880@gmail.com>
2026-01-08 16:25:15 +0800

    Fix os import in settings.py

--- tartanilla_admin/settings.py
+++ tartanilla_admin/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -242,11 +247,7 @@ WSGI_APPLICATION = 'tartanilla_admin.wsgi.application'
 
 
 
-import os
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv(BASE_DIR / '.env')
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
